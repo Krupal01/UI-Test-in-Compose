2343cd1bda8c4a42ad52ea025fd524a1282a14ce
krupal <bhuvakrupal01@gmail.com>
2022-07-21 15:45:45 +0530

    ADD : Toast test

--- app/src/androidTest/java/com/example/composetesting/MainScreenTest.kt
+++ app/src/androidTest/java/com/example/composetesting/MainScreenTest.kt
@@ -1,10 +1,18 @@
 package com.example.composetesting
 
+import android.os.IBinder
+import android.view.WindowManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Root
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.example.composetesting.ui.theme.ComposeTestingTheme
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -44,4 +52,30 @@ class MainScreenTest {
         submitBtn.performClick()
 
     }
+
+    @Test
+    fun toastTest(){
+        Espresso.onView(withText(TOAST_MSG)).inRoot(ToastMatcher())
+            .check(matches(isDisplayed()))
+    }
+}
+
+class ToastMatcher : TypeSafeMatcher<Root?>() {
+
+    override fun describeTo(description: org.hamcrest.Description?) {
+        description?.appendText("is toast")
+    }
+
+    override fun matchesSafely(item: Root?): Boolean {
+        val type: Int? = item?.windowLayoutParams?.get()?.type
+        if (type == WindowManager.LayoutParams.TYPE_TOAST) {
+            val windowToken: IBinder = item.decorView.windowToken
+            val appToken: IBinder = item.decorView.applicationWindowToken
+            if (windowToken === appToken) {
+                return true
+            }
+        }
+        return false
+    }
+
 }
